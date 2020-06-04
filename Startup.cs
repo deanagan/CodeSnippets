aa09b0af977ff2e9bbe78e4e22d597e1eecf15ee
Dean Agan <agandfr@gmail.com>
2020-06-04 17:33:17 +1000

    Add status code pages. We want to customise this bit someday.

--- Startup.cs
+++ Startup.cs
@@ -39,6 +39,7 @@ namespace CodeSnippets
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePages();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
