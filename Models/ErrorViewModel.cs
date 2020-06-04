7883376142262171c8c82ba607208e0eb1b8d3b2
Dean Agan <agandfr@gmail.com>
2020-06-04 17:17:01 +1000

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeSnippets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
